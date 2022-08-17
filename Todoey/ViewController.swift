96c9c244c83c10f85f48ea88bd5ae551afeb6312
Mike Schmidt <schmidt217@gmail.com>
2022-08-17 14:00:13 -0600

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Michael Schmidt on 8/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
